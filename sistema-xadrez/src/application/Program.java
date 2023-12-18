5b9ccd43b0d2452996ba455621cd870e9fa1ac29
Yuri Carias <yuristricken7@gmail.com>
2023-12-17 21:41:49 -0300

    Movendo peças

--- sistema-xadrez/src/application/Program.java
+++ sistema-xadrez/src/application/Program.java
@@ -2,12 +2,30 @@ package application;
 
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Origem: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Alvo: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+
 
     }
 }
--- sistema-xadrez/src/application/UI.java
+++ sistema-xadrez/src/application/UI.java
@@ -2,7 +2,11 @@ package application;
 
 import chess.ChessPiece;
 
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -26,6 +30,18 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }
+        catch (RuntimeException e) {
+            throw new InputMismatchException("Erro lendo a posição do xadrez. Valores validos são de a1 até h8");
+        }
+    }
 
 
     public static  void printBoard(ChessPiece[][] pieces) {
