2d4bcb21be898d059f9c2aad1498f9a42de4fca5
Yuri Carias <yuristricken7@gmail.com>
2023-12-22 18:15:36 -0300

    Lógica de Check

--- sistema-xadrez/src/chess/ChessPiece.java
+++ sistema-xadrez/src/chess/ChessPiece.java
@@ -14,7 +14,12 @@ public abstract class ChessPiece extends Piece {
     }
 
     public Color getColor() {
+
         return color;
+    }
+
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);
     }
 
     protected boolean isThereOpponentPiece(Position position) {
